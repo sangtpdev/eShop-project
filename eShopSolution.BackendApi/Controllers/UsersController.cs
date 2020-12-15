994e79a1da7f339bcb84fad4d3c8389ea8b170e5
PhamTanSang <phamsang71@gmail.com>
2020-12-15 22:45:04 +0700

    update login

---
+++ eShopSolution.BackendApi/Controllers/UsersController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eShopSolution.Application.System.Users;
+using eShopSolution.ViewModels.System.Users;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShopSolution.BackendApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userServive;
+        public UsersController(IUserService userService)
+        {
+            _userServive = userService;
+        }
+
+        [HttpPost("authenticate")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Authenticate([FromForm]LoginRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var resultToken = await _userServive.Authencate(request);
+            if (string.IsNullOrEmpty(resultToken))
+            {
+                return BadRequest("Username or passwork is incorrect");
+            }
+            return Ok(new { token = resultToken });
+        }
+
+
+        [HttpPost("register")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register([FromForm]RegisterRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var result = await _userServive.Register(request);
+            if (!result)
+            {
+                return BadRequest("Register is Unsuccessful.");
+            }
+            return Ok();
+        }
+    }
+}
